7b3a286337a6670e43698d4f9468ac73864d113a
Branddoon <jairgrande.22@gmail.com>
2025-06-22 16:38:56 -0600

    Feat: Added buttons design page

--- lib/presentation/buttons/buttons.dart
+++ lib/presentation/buttons/buttons.dart
@@ -9,7 +9,8 @@ class ButtonsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Buttons'),
-      )
+      ),
+      body: ButtonView()
     );
   }
 }
@@ -19,9 +20,20 @@ class ButtonView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Wrap(
       children: [
         ElevatedButton(onPressed: (){}, child: Text('Elevated button')),
+        ElevatedButton(onPressed: null, child: Text('Elevated button disabled')),
+        ElevatedButton.icon(onPressed: (){}, label: Text('Filled icon'), icon: Icon(Icons.access_alarm)),
+        FilledButton(onPressed: (){}, child: Text('Filled button')),
+        FilledButton.icon(onPressed: (){}, icon: const Icon(Icons.accessibility_new), label: Text('Filled icon')),
+        OutlinedButton(onPressed: (){}, child: Text('Outline')),
+        OutlinedButton.icon(onPressed: (){}, label: Text('Outline button'), icon: Icon(Icons.terminal),),
+        TextButton(onPressed: (){}, child: Text('Text button')),
+        TextButton(onPressed: (){}, style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.amber)),child: Text('Text button with color')),
+        TextButton.icon(onPressed: (){}, label: Text('Text button with icon'), icon: Icon(Icons.abc_outlined),),
+        IconButton(onPressed: (){}, icon: Icon(Icons.access_time_filled)),
+        IconButton(onPressed: (){},  icon: Icon(Icons.vaccines), style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.amber)),),
       ],
     );
   }
