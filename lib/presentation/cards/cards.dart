52ca8e7600b98c7e2fa49206f9dc54725c88395f
Branddoon <jairgrande.22@gmail.com>
2025-06-22 17:12:43 -0600

    Feat: Added card design page

--- lib/presentation/cards/cards.dart
+++ lib/presentation/cards/cards.dart
@@ -9,6 +9,42 @@ class CardScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
+      body: CardView(),
+    );
+  }
+}
+
+class CardView extends StatelessWidget {
+  const CardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        color: Colors.grey,
+        child: SizedBox(
+          width: 350,
+          height: 450,
+          child: Padding(
+              padding: EdgeInsetsGeometry.all(10.0),
+              child: Column(
+                children: [
+                  CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 120,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                            "https://www.grupoendor.com/wp-content/uploads/2024/07/default-avatar-icon-of-social-media-user-vector.jpg"), //NetworkImage
+                        radius: 100,
+                      ), //CircleAvatar
+                  ),
+                  Align(child: Text('User', style: TextStyle(color: Colors.white, fontSize: 30),),),
+                  Align(child: Text('Email', style: TextStyle(color: Colors.white, fontSize: 20),),)
+                ],
+              ),
+            ),
+        ),
+      ),
     );
   }
 }
